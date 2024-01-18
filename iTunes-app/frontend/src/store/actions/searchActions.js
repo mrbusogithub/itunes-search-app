5ed4b32151be0c462a7ce728aff3921111ca891e
mrbusogithub <sonwabisob@gmail.com>
2024-01-18 20:29:00 +0200

    updated backend URL

--- iTunes-app/frontend/src/store/actions/searchActions.js
+++ iTunes-app/frontend/src/store/actions/searchActions.js
@@ -4,10 +4,13 @@ import axios from "axios";
 export const searchMusic = (term, mediaType) => async (dispatch) => {
   try {
     // Make an asynchronous request to the music search API
-    const response = await axios.get(`http://localhost:5000/api/search/music`, {
-      params: { term, mediaType },
-      headers: { Authorization: "" },
-    });
+    const response = await axios.get(
+      `https://itunes-search-music.onrender.com/api/search/music`,
+      {
+        params: { term, mediaType },
+        headers: { Authorization: "" },
+      }
+    );
 
     // Dispatch a search success action with the retrieved albums
     dispatch({ type: "SEARCH_SUCCESS", payload: response.data.albums });
